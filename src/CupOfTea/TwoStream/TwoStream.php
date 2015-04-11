966d6c5af7eefd30be94c20aad0e797c5d2d9648
CupOfTea696 <cupoftea696@gmail.com>
2015-04-11 13:38:12 +0100

    Release: v0.0.4-alpha

--- src/CupOfTea/TwoStream/TwoStream.php
+++ src/CupOfTea/TwoStream/TwoStream.php
@@ -14,7 +14,7 @@ class TwoStream implements ProviderContract{
      *
      */
     const PACKAGE = 'CupOfTea/TwoStream';
-    const VERSION = '0.0.3-alpha';
+    const VERSION = '0.0.4-alpha';
     
 	/**
 	 * This package's configuration
