6c72594e5278e7fc0616c13278658b39454f1e50
CupOfTea696 <cupoftea696@gmail.com>
2015-04-25 16:03:21 +0100

    ControllerInspector

---
+++ src/Routing/ControllerInspector.php
@@ -0,0 +1,14 @@
+<?php namespace CupOfTea\TwoStream\Routing;
+
+use Illuminate\Routing\ControllerInspector as LaravelControllerInspector;
+
+class ControllerInspector extends LaravelControllerInspector{
+    
+    /**
+     * {@inheritdoc}
+     */
+    protected $verbs = array(
+		'call', 'publish', 'subscribe', 'unsubscribe',
+	);
+    
+}
