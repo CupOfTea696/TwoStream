2810c6f7d38d4567a17109535899567ed2e14aec
CupOfTea696 <cupoftea696@gmail.com>
2015-04-25 16:23:29 +0100

    Release: v0.0.13-alpha

--- src/TwoStream.php
+++ src/TwoStream.php
@@ -56,7 +56,7 @@ class TwoStream implements ProviderContract{
      *
      */
     const PACKAGE = 'CupOfTea/TwoStream';
-    const VERSION = '0.0.12-alpha';
+    const VERSION = '0.0.13-alpha';
     
     /**
      * This package's configuration
