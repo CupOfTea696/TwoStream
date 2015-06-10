95d9f7865156f031cf89242e9cd78fb6a1883c78
CupOfTea696 <cupoftea696@gmail.com>
2015-06-10 15:06:58 +0200

    Release: v0.2.7-beta

--- src/TwoStream.php
+++ src/TwoStream.php
@@ -60,7 +60,7 @@ class TwoStream implements ProviderContract
      * @const string
      */
     const PACKAGE = 'CupOfTea/TwoStream';
-    const VERSION = '0.2.6-beta';
+    const VERSION = '0.2.7-beta';
     
     /**
      * Socket Pull ID
